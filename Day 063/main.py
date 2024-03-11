af1a8a5311e8cf21c037b430be3747a667a7306f
Steven Tey <stevenweijun1@gmail.com>
2024-03-11 09:34:40 +0800

    ~

--- Day 063/main.py
+++ Day 063/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -14,40 +17,64 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+class Base(DeclarativeBase):
+    pass
+
+
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy(model_class=Base)
+# initialize the app with the extension
+db.init_app(app)
+
+class Book(db.Model):
+    __tablename__ = "books"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+# with app.app_context():
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
 
 # Initiate a connection to the database
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS books \
-    (id INTEGER PRIMARY KEY, \
-    title varchar(250) NOT NULL UNIQUE, \
-    author varchar(250) NOT NULL, \
-    rating FLOAT NOT NULL)")
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# cursor.execute("CREATE TABLE IF NOT EXISTS books \
+#     (id INTEGER PRIMARY KEY, \
+#     title varchar(250) NOT NULL UNIQUE, \
+#     author varchar(250) NOT NULL, \
+#     rating FLOAT NOT NULL)")
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
 
-all_books = []
-
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    all_books = result.scalars()
+    return render_template("index.html", books=all_books.all())
 
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
     if request.method == "POST":
-        book_name = request.form["book_name"]
-        book_author = request.form["book_author"]
-        rating = request.form["rating"]
-        new_book = {
-            "name": book_name,
-            "author": book_author,
-            "rating": rating
-        }
-        all_books.append(new_book)
-        print(all_books)
+        # Create Record
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
