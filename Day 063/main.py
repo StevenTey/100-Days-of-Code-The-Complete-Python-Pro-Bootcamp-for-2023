876e1eb2014db50364b494c39b3be7caa77c2314
Steven Tey <stevenweijun1@gmail.com>
2024-03-11 21:59:23 +0800

    One more day

--- Day 063/main.py
+++ Day 063/main.py
@@ -69,14 +69,34 @@ def add():
     if request.method == "POST":
         # Create Record
         new_book = Book(
-            title=request.form["title"],
-            author=request.form["author"],
+            title=request.form["book_name"],
+            author=request.form["book_author"],
             rating=request.form["rating"]
         )
         db.session.add(new_book)
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get("id")
+    book_selected = Book.query.get(book_id)
+    return render_template("edit.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id")
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run()
