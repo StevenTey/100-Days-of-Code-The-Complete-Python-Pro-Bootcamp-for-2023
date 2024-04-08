3646ebebf33f9a2c9fe5b2b5a427703d9bf4b19e
Steven Tey <stevenweijun1@gmail.com>
2024-04-08 09:41:43 +0800

    Why the packages don't work

--- Day 064/main.py
+++ Day 064/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
@@ -23,12 +23,28 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+Bootstrap(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-movies-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 # CREATE TABLE
+class Movie(db.model):
+    __tablename__ = "movies"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 @app.route("/")
